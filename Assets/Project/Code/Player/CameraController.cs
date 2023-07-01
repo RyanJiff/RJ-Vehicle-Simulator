43586070ddaf873eedd52dac0299c867086b0493
RyanJiff <40864308+RyanJiff@users.noreply.github.com>
2023-07-01 05:56:11 +0300

    Update CameraController.cs
    Late update should only be responsible for switching camera modes and checking if we have a valid target

--- Assets/Project/Code/Player/CameraController.cs
+++ Assets/Project/Code/Player/CameraController.cs
@@ -25,12 +25,12 @@ public class CameraController : MonoBehaviour
     // Orbit camera DEV
     public float distanceFromCenterOrbit = 8f;
     public float lookAroundSpeed = 20f;
-    public Vector3 OffsetOribt = Vector3.zero;
+    public Vector3 OffsetOrbit = Vector3.zero;
 
     private Vector3 nextPosRig;
     private Vector3 nextPosCam;
-    [SerializeField] private float RotAroundY = -90f;
-    [SerializeField] private float RotAroundXZ = 75f;
+    private float RotAroundY = -90f;
+    private float RotAroundXZ = 75f;
 
     int i = 0;
 
@@ -57,65 +57,11 @@ public class CameraController : MonoBehaviour
 
             if (cameraMode == CameraMode.normal)
             {
-                cameraTransform.localPosition = Vector3.zero;
-                cameraRigTransform.rotation = Quaternion.identity;
-
-                if (Input.GetKeyDown(KeyCode.V))
-                {
-                    i = (i + 1) % offsets.Count;
-                }
-                
-                if (Input.GetKey(KeyCode.C))
-                {
-                    nextPosRig = target.forward * distances[i] + target.position + target.TransformDirection(offsets[i]);
-                }
-                else
-                {
-                    nextPosRig = target.forward * -distances[i] + target.position + target.TransformDirection(offsets[i]);
-                }
-
-                cameraRigTransform.position = nextPosRig;
-                cameraTransform.LookAt(target.position + target.up * lookOffsets[i].y + target.right * -lookOffsets[i].x + target.forward * lookOffsets[i].z, target.up);
+                NormalCamera();
             }
             else if(cameraMode == CameraMode.orbit)
             {
-                /*
-                 *  x = r * cos(s) * sin(t)
-                    y = r * sin(s) * sin(t)
-                    z = r * cos(t)
-                    here, s is the angle around the z-axis, and t is the height angle, measured 'down' from the z-axis.
-
-                    x=rsinθ, y=rcosθ
-                */
-                if (Input.GetKey(KeyCode.Keypad8))
-                {
-                    RotAroundXZ = RotAroundXZ - lookAroundSpeed * Time.deltaTime;
-                }
-                else if (Input.GetKey(KeyCode.Keypad2))
-                {
-                    RotAroundXZ = RotAroundXZ + lookAroundSpeed * Time.deltaTime;
-                }
-                RotAroundXZ = Mathf.Clamp(RotAroundXZ, 5f, 175f);
-
-                if (Input.GetKey(KeyCode.Keypad6))
-                {
-                    RotAroundY = RotAroundY + lookAroundSpeed * 2 * Time.deltaTime;
-                }
-                else if (Input.GetKey(KeyCode.Keypad4))
-                {
-                    RotAroundY = RotAroundY - lookAroundSpeed * 2 * Time.deltaTime;
-                }
-
-                nextPosRig = target.position + target.up * OffsetOribt.y + target.right * -OffsetOribt.x + target.forward * OffsetOribt.z;
-
-                float XPosCam = distanceFromCenterOrbit * Mathf.Cos(RotAroundY * Mathf.Deg2Rad) * Mathf.Sin(RotAroundXZ * Mathf.Deg2Rad);
-                float ZPosCam = distanceFromCenterOrbit * Mathf.Sin(RotAroundY * Mathf.Deg2Rad) * Mathf.Sin(RotAroundXZ * Mathf.Deg2Rad);
-                float YPosCam = distanceFromCenterOrbit * Mathf.Cos(RotAroundXZ * Mathf.Deg2Rad);
-                nextPosCam = new Vector3(XPosCam, YPosCam, ZPosCam);
-
-                cameraTransform.localPosition = nextPosCam;
-                cameraRigTransform.position = nextPosRig;
-                cameraTransform.LookAt(target.position + target.up * OffsetOribt.y + target.right * -OffsetOribt.x + target.forward * OffsetOribt.z, Vector3.up);
+                OrbitCamera();
             }
         }
     }
@@ -146,5 +92,68 @@ public class CameraController : MonoBehaviour
             return cameraRigTransform;
         }
         return null;
+    }
+
+    private void OrbitCamera()
+    {
+        /*
+            *  x = r * cos(s) * sin(t)
+            y = r * sin(s) * sin(t)
+            z = r * cos(t)
+            here, s is the angle around the z-axis, and t is the height angle, measured 'down' from the z-axis.
+
+            x=rsinθ, y=rcosθ
+        */
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            RotAroundXZ = RotAroundXZ - lookAroundSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.Keypad2))
+        {
+            RotAroundXZ = RotAroundXZ + lookAroundSpeed * Time.deltaTime;
+        }
+        RotAroundXZ = Mathf.Clamp(RotAroundXZ, 5f, 175f);
+
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            RotAroundY = RotAroundY + lookAroundSpeed * 2 * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.Keypad4))
+        {
+            RotAroundY = RotAroundY - lookAroundSpeed * 2 * Time.deltaTime;
+        }
+
+        nextPosRig = target.position + target.up * OffsetOrbit.y + target.right * -OffsetOrbit.x + target.forward * OffsetOrbit.z;
+
+        float XPosCam = distanceFromCenterOrbit * Mathf.Cos(RotAroundY * Mathf.Deg2Rad) * Mathf.Sin(RotAroundXZ * Mathf.Deg2Rad);
+        float ZPosCam = distanceFromCenterOrbit * Mathf.Sin(RotAroundY * Mathf.Deg2Rad) * Mathf.Sin(RotAroundXZ * Mathf.Deg2Rad);
+        float YPosCam = distanceFromCenterOrbit * Mathf.Cos(RotAroundXZ * Mathf.Deg2Rad);
+        nextPosCam = new Vector3(XPosCam, YPosCam, ZPosCam);
+
+        cameraTransform.localPosition = nextPosCam;
+        cameraRigTransform.position = nextPosRig;
+        cameraTransform.LookAt(target.position + target.up * OffsetOrbit.y + target.right * -OffsetOrbit.x + target.forward * OffsetOrbit.z, Vector3.up);
+    }
+    private void NormalCamera()
+    {
+        cameraTransform.localPosition = Vector3.zero;
+        cameraRigTransform.rotation = Quaternion.identity;
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            i = (i + 1) % offsets.Count;
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            nextPosRig = target.forward * distances[i] + target.position + target.TransformDirection(offsets[i]);
+        }
+        else
+        {
+            nextPosRig = target.forward * -distances[i] + target.position + target.TransformDirection(offsets[i]);
+        }
+
+        cameraRigTransform.position = nextPosRig;
+        cameraTransform.LookAt(target.position + target.up * lookOffsets[i].y + target.right * -lookOffsets[i].x + target.forward * lookOffsets[i].z, target.up);
     }
 }
